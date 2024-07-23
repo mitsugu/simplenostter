d7e2735a87fc2c6ed503c1cec334a1fa04b2909e
mitsugu oyama <mitsugu.oyama@gmail.com>
2024-07-24 04:44:34 +0900

    first release

--- resource/options.js
+++ resource/options.js
@@ -1,4 +1,34 @@
 (() => {
+	var loadTrendSettingData = () => {
+		return new Promise((resolve, reject) => {
+			browser.storage.sync.get("trend").then(
+				(item) => {
+					document.querySelector("#removetrend").checked = item.trend;
+					resolve("get trend status");
+				},
+				(error) => {
+					document.querySelector("#removetrend").checked = true;
+					resolve("default set trend status");
+				}
+			);
+		});
+	}
+
+	var loadKind67SettingData = () => {
+		return new Promise((resolve, reject) => {
+			browser.storage.sync.get("kind67").then(
+				(item) => {
+					document.querySelector("#removekind67").checked = item.kind67;
+					resolve("get kind67 status");
+				},
+				(error) => {
+					document.querySelector("#removekind67").checked = true;
+					resolve("default set kind67 status");
+				}
+			);
+		});
+	}
+
 	document.addEventListener("DOMContentLoaded", () => {
 		document.querySelector("#removetrend").addEventListener(
 			"click",
@@ -24,4 +54,22 @@
 			}
 		);
 	});
+
+	loadTrendSettingData()
+		.then(result => {
+			loadKind67SettingData()
+				.then(result => {})
+				.catch(error => {
+					console.error(
+						"[addon simple nostter] cannot read kind67 status",
+						error
+					)
+				});
+		})
+		.catch(error => {
+			console.error(
+				"[addon simple nostter] cannot read trend status",
+				error
+			)
+		});
 })();
